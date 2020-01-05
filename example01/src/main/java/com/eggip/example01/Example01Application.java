eefbf0153e33ab46d5cd4ca81416d75fa3b60d8b
renjiyun <wozaijixu@msn.cn>
2020-01-05 16:00:26 +0800

    新增example02

--- example01/src/main/java/com/eggip/example01/Example01Application.java
+++ example01/src/main/java/com/eggip/example01/Example01Application.java
@@ -2,12 +2,22 @@ package com.eggip.example01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Example01Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Example01Application.class, args);
+    }
+
+    @GetMapping("/sayHello")
+    @ResponseBody
+    public String sayHello() {
+        return "hello";
     }
 
 }
